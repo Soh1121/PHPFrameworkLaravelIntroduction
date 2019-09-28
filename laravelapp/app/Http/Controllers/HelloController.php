4fe8e1b5fb5164a69ed7c690adee824f9b81f388
Soh1121 <satou.shg@gmail.com>
2019-09-28 16:14:54 +0900

    [update]２つ目のアクションを用意
    複数のアクションの動作を理解するために

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -36,4 +36,11 @@ class HelloController extends Controller
         $html = $head . tag('title', 'Hello/Index') . $style . $body . tag('h1', 'Index') . tag('p', 'this is Index page') . '<a href="/hello/other">go to Other page</a>' . $end;
         return $html;
     }
+
+    public function other() {
+        global $head, $style, $body, $end;
+
+        $html = $head . tag('title', 'Hello/Other') . $style . $body . tag('h1', 'Other') . tag('p', 'this is Other page') . $end;
+        return $html;
+    }
 }
