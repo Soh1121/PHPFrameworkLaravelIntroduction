613f236f2ee63a215a3046d8f20ace33ec9736ed
Soh1121 <satou.shg@gmail.com>
2019-09-28 16:12:31 +0900

    [update]グローバル変数とindexメソッドを用意
    複数アクションを理解するために１つ目を作成

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -4,13 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HelloController extends Controller
-{
-    public function index($id='noname', $pass='unknown') {
-        return <<<EOF
-<html>
-<head>
-<title>Hello/Index</title>
+global $head, $style, $body, $end;
+$head = '<html><head>';
+$style = <<<EOF
 <style>
 body {
     font-size: 16pt;
@@ -21,18 +17,23 @@ h1 {
     text-align: right;
     color: #eee;
     margin: -40px 0 -50px 0;
+}
 </style>
-</head>
-<body>
-    <h1>Index</h1>
-    <p>これは、Helloコントローラのindexアクションです。</p>
-    <ul>
-        <li>ID: {$id}</li>
-        <li>PASS: {$pass}</li>
-    </ul>
-</body>
-</html>
 EOF;
+$body = '</head><body>';
+$end = '</body></html>';
 
+function tag($tag, $txt)
+{
+    return "<{$tag}>" . $txt . "</{$tag}>";
+}
+
+class HelloController extends Controller
+{
+    public function index() {
+        global $head, $style, $body, $end;
+
+        $html = $head . tag('title', 'Hello/Index') . $style . $body . tag('h1', 'Index') . tag('p', 'this is Index page') . '<a href="/hello/other">go to Other page</a>' . $end;
+        return $html;
     }
 }
