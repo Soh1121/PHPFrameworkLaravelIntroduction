584b8d7c8062eb01347568e4964ff8a4b677f53a
Soh1121 <satou.shg@gmail.com>
2019-10-22 17:42:16 +0900

    [update]グローバルミドルウェアからグループミドルウェアに変更
    グループミドルウェアの動作を確認するため

--- laravelapp/app/Http/Kernel.php
+++ laravelapp/app/Http/Kernel.php
@@ -19,7 +19,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\HelloMiddleware::class,
     ];
 
     /**
@@ -42,6 +41,9 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             'bindings',
         ],
+        'hello' => [
+            \App\Http\Middleware\HelloMiddleware::class,
+        ]
     ];
 
     /**
