9d24131c9912510e277beea90e5081fbe049ee8a
Soh1121 <satou.shg@gmail.com>
2019-10-22 16:52:53 +0900

    [update]ミドルウェアを実装
    ミドルウェアの動作確認を行うため

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Response;
 class HelloController extends Controller
 {
     public function index() {
-        return view('hello.index', ['message'=>'Hello!']);
+        return view('hello.index', ['data'=>$request->data]);
     }
 }
