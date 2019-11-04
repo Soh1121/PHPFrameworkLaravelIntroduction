cc2ee662e5270baf05c91314c1cf05566877eed4
Soh1121 <satou.shg@gmail.com>
2019-11-04 20:53:42 +0900

    [fix]use宣言を修正
    誤植だったため

--- laravelapp/app/Providers/HelloServiceProvider.php
+++ laravelapp/app/Providers/HelloServiceProvider.php
@@ -3,7 +3,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Validation\Validator;
+use Validator;
 use App\Http\Validators\HelloValidator;
 
 class HelloServiceProvider extends ServiceProvider
