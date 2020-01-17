641d8a2473cbc70161511e522547e8ad301129a8
Soh1121 <satou.shg@gmail.com>
2020-01-17 21:36:24 +0900

    [update]コントローラーをpeopleテーブルからデータを取得するように変更
    DBクラスを学習するため

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -4,26 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 use App\Http\Requests\HelloRequest;
 use Validator;
 
-class HelloController extends Controller {
-    public function index(Request $request) {
-        if ($request->hasCookie('msg')) {
-            $msg = 'Cookie: ' . $request->cookie('msg');
-        } else {
-            $msg = '※クッキーはありません。';
-        }
-        return view('hello.index', ['msg' => $msg]);
+class HelloController extends Controller
+{
+    public function index(Request $request)
+    {
+        $items = DB::select('select * from people');
+        return view('hello.index', ['items' => $items]);
     }
-
-    public function post(Request $request) {
+    public function post(Request $request)
+    {
         $validate_rule = [
             'msg' => 'required',
         ];
         $this->validate($request, $validate_rule);
         $msg = $request->msg;
-        $response = new Response(view('hello.index', ['msg'=>'「' . $msg .'」をクッキーに保存しました。']));
+        $response = new Response(view('hello.index', ['msg' => '「' . $msg . '」をクッキーに保存しました。']));
         $response->cookie('msg', $msg, 100);
         return $response;
     }
